7995379648a6948746635f94883a950300b94ec9
Zili Wang <ziliwang912@gmail.com>
2020-02-05 20:51:28 -0500

    format code

--- LinkedList.java
+++ LinkedList.java
@@ -16,6 +16,7 @@ public class LinkedList<E> {
 
     /**
      * Checks if this list is empty
+     * 
      * @return true if this list is empty
      */
     public boolean isEmpty() {
@@ -24,18 +25,17 @@ public class LinkedList<E> {
 
     /**
      * Adds element to the end of this list
-     * @param element 
+     * 
+     * @param element
      */
     public void add(E element) {
         if (head == null) {
             head = new Node<E>(element, null);
         } else {
             Node<E> tmp = head;
-
             while (tmp.next != null) {
                 tmp = tmp.next;
             }
-
             tmp.next = new Node<E>(element, null);
         }
     }
@@ -48,7 +48,6 @@ public class LinkedList<E> {
         if (isEmpty()) {
             throw new IndexOutOfBoundsException();
         }
-
         Node<E> tmp = head;
         for (int i = 0; i < index; i++) {
             tmp = tmp.next;
@@ -56,12 +55,12 @@ public class LinkedList<E> {
         if (tmp == null) {
             throw new IndexOutOfBoundsException();
         }
-
         return tmp.element;
     }
 
     /**
-     * Removes and returns the element at the specified position in this list 
+     * Removes and returns the element at the specified position in this list
+     * 
      * @param index
      * @return the element at the specified position in this list
      */
@@ -69,13 +68,11 @@ public class LinkedList<E> {
         if (isEmpty() || index > size() - 1) {
             throw new IndexOutOfBoundsException();
         }
-
         if (index == 0) {
             E tmp = head.element;
             head = head.next;
             return tmp;
-        } 
-
+        }
         Node<E> curr = head;
         Node<E> prev = null;
         for (int i = 0; i < index; i++) {
@@ -83,7 +80,6 @@ public class LinkedList<E> {
             curr = curr.next;
         }
         prev.next = curr.next;
-
         return curr.element;
     }
 
@@ -96,6 +92,7 @@ public class LinkedList<E> {
 
     /**
      * Returns the size of this list
+     * 
      * @return the number of elements in this list
      */
     public int size() {
@@ -106,12 +103,10 @@ public class LinkedList<E> {
         } else {
             int size = 1;
             Node<E> tmp = head;
-
             while (tmp.next != null) {
                 size++;
                 tmp = tmp.next;
             }
-
             return size;
         }
     }
@@ -125,7 +120,7 @@ public class LinkedList<E> {
         while (tmp != null) {
             result.append(tmp.element + " ");
             tmp = tmp.next;
-        } 
+        }
         return result.toString();
     }
 
@@ -137,35 +132,35 @@ public class LinkedList<E> {
     private static class Node<E> {
         private E element;
         private Node<E> next;
-        
+
         /**
-         * Constructs a node with 
-         * @param element 
-         * @param next pointer pointing to the next node
+         * Constructs a node with
+         * 
+         * @param element
+         * @param next    pointer pointing to the next node
          */
         public Node(E element, Node<E> next) {
             this.element = element;
             this.next = next;
-        } 
+        }
     }
 
-	public static void main(String args[]) {      
+    public static void main(String args[]) {
         LinkedList<Integer> myList = new LinkedList<>();
-        System.out.println(myList.isEmpty());   // true
+        System.out.println(myList.isEmpty()); 
         myList.add(100);
         System.out.println(myList.toString());
-        System.out.println("Size = " + myList.size());  
+        System.out.println("Size = " + myList.size());
         myList.remove(0);
         myList.add(-23);
         myList.add(17);
         myList.add(100);
         System.out.println("myList: " + myList.toString());
-        System.out.println("Size = " + myList.size());  
+        System.out.println("Size = " + myList.size());
         myList.remove(1);
         System.out.println("myList: " + myList.toString());
-        System.out.println("Size = " + myList.size());  
+        System.out.println("Size = " + myList.size());
         myList.clear();
-        System.out.println("Size = " + myList.size()); 
-	}
+        System.out.println("Size = " + myList.size());
+    }
 }
-
